258c48bbca2e7b74477708699ce6620ba3d0c73c
toannvit <toannvit@gmail.com>
2019-03-24 14:10:56 +0700

    Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Toan Ngo
                 </div>
 
                 <div class="links">
